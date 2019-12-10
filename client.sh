512f108ffb43c382fccd7d880e7b928fe0f46e01
Patryk Gronkiewicz <gronek.konta@gmail.com>
2019-12-10 17:33:01 +0100

    init

---
+++ client.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl https://patchbay.pub/twojastaradupa
+notify-send "Job done"
---
+++ server.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+runipy u.ipnyb o.ipnyb
+curl -X POST https://patchbay.pub/twojastaradupa
+
